9bba4dd995894f3c751605a3df8dddd4de5796ea
Ronan Williams <ronan@ceterna.com>
2019-09-10 22:09:29 +0100

    10.09.19 3

--- src/aura/QuoteHolder/QuoteHolderController.js
+++ src/aura/QuoteHolder/QuoteHolderController.js
@@ -23,6 +23,7 @@
 
                 Object.keys(response.getReturnValue()['FAMILYGROUPS']).forEach(function(group){
 
+                    console.log('processing ' + group);
 
                     var family      = {};
                     family.Name     = group;
@@ -34,21 +35,15 @@
                     family.Total    = 0;
                     family.UseCases = [];
 
-                    // console.log(response.getReturnValue()['FAMILYGROUPS'][group]);
-
                     Object.keys(response.getReturnValue()['FAMILYGROUPS'][group]).forEach(function(caseName){
-
-                        // console.log('case name is ' + caseName);
-
-
                         var useCase         = {};
                         useCase.Name        = caseName;
-                        // useCase.Year1       = 0;
-                        // useCase.Year2       = 0;
-                        // useCase.Year3       = 0;
-                        // useCase.Year4       = 0;
-                        // useCase.Year5       = 0;
-                        // useCase.Total       = 0;
+                        useCase.Year1       = 0;
+                        useCase.Year2       = 0;
+                        useCase.Year3       = 0;
+                        useCase.Year4       = 0;
+                        useCase.Year5       = 0;
+                        useCase.Total       = 0;
                         useCase.Products    = [];
 
                         Object.keys(response.getReturnValue()['FAMILYGROUPS'][group][caseName]).forEach(function(product){
@@ -57,7 +52,8 @@
                             prod.UseCase            = caseName;
                             prod.Id                 = product.split(':')[2];
                             prod.PricebookEntryId   = product.split(':')[3];
-                            prod.Name               = product.split(':')[0] + ': ' + product.split(':')[1];
+                            // prod.Name               = product.split(':')[0] + ': ' + product.split(':')[1];
+                            prod.Name               = product.split(':')[1];
                             prod.Entries            = response.getReturnValue()['FAMILYGROUPS'][group][caseName][product];
                             prod.DiscountPercent    = false;
                             useCase.Products.push(prod);
@@ -65,15 +61,13 @@
                             prod.Entries.forEach(function (entry){
                                 if (entry.Year__c){
                                     family['Year' + entry.Year__c] += entry.TotalPrice;
+                                    family.Total += entry.TotalPrice;
+                                    useCase['Year' + entry.Year__c] += entry.TotalPrice;
+                                    useCase.Total += entry.TotalPrice;
                                 }
                                console.log(entry);
                             });
-
-                            // productLines.push(product);
                         });
-
-                        // console.log('products');
-                        // console.log(useCase.Products);
 
                         family.UseCases.push(useCase);
                     });
@@ -92,6 +86,47 @@
         $A.enqueueAction(getInfo);
 
     },
+    createFamily : function($C,$E,$H){
+
+        var familyName  = $C.find('codelistInput').getElement().value;
+        var familyGroups = $C.get('v.familyGroups');
+        var family      = {};
+        family.Name     = familyName;
+        family.Year1    = 0;
+        family.Year2    = 0;
+        family.Year3    = 0;
+        family.Year4    = 0;
+        family.Year5    = 0;
+        family.Total    = 0;
+        family.UseCases = [];
+        familyGroups.push(family);
+
+        $C.set('v.familyGroups',familyGroups);
+        $C.find('codelistInput').getElement().value = null;
+
+        var families = $C.get('v.families');
+        for (var x = 0; x < families.length; x++){
+            if (families[x] === familyName){
+                families.splice(x,1);
+            }
+        }
+        $C.set('v.families',families);
+
+    },
+    addUseCase : function($C,$E,$H){
+
+        var useCase = $E.currentTarget.value;
+        var familyIndex = $E.currentTarget.dataset.familyindex;
+
+        var familyGroups = $C.get('v.familyGroups');
+
+        familyGroups[familyIndex].UseCases.push({});
+
+        $C.set('v.familyGroups',familyGroups);
+        console.log($E.currentTarget.value);
+
+    },
+
     addProductLine : function($C,$E,$H){
 
 
@@ -139,13 +174,12 @@
 
         var newLineItem = {
 
-            Id: "00k0E000004wWvwQAE",
-            Name: "testSaasLicense1 Correspondent Banking AML",
-            PricebookEntryId: "01u0Y000004JA4AQAW",
-            Product2Id: "01t0Y000005LxpyQAC",
-            Product_Code__c: "IG",
+            // Id: "00k0E000004wWvwQAE",
+            // Name: "testSaasLicense1 Correspondent Banking AML",
+            // PricebookEntryId: "01u0Y000004JA4AQAW",
+            // Product2Id: "01t0Y000005LxpyQAC",
+            // Product_Code__c: "IG",
             Quantity: 1,
-            Term_Years__c: "3",
             TotalPrice: 100,
             UnitPrice: 100,
             UsesAmount__c: false
@@ -160,23 +194,26 @@
 
         // console.log(source);
 
-        // var insert = $C.get('c.insertLineItem');
-        // insert.setParams({
-        //     recordId : $C.get('v.recordId'),
-        //     productId : sourceData.id,
-        //     pricebookEntryId : sourceData.pricebookEntryId,
-        //     year : sourceData.week
-        // });
-        // insert.setCallback(this,function (response) {
-        //
-        //
-        //     console.log(response.getReturnValue());
-        //
-        //
-        //
-        // });
-        //
-        // $A.enqueueAction(insert);
+        var insert = $C.get('c.insertLineItem');
+        insert.setParams({
+            recordId : $C.get('v.recordId'),
+            productId : sourceData.id,
+            pricebookEntryId : sourceData.pricebookid,
+            year : sourceData.week
+        });
+        insert.setCallback(this,function (response) {
+
+
+            familyGroups[sourceData.familyindex].UseCases[sourceData.usecaseindex].Products[sourceData.productindex].Entries[sourceData.week -2] = response.getReturnValue();
+
+            console.log(response.getReturnValue());
+
+            $C.set('v.familyGroups',familyGroups);
+
+
+        });
+
+        $A.enqueueAction(insert);
     },
     setUsesAmount : function($C,$E,$H) {
         var sourceData = $E.currentTarget.dataset;
