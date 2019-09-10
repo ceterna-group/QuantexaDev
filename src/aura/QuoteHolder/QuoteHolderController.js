fc4c8a89573382c7effb98ddf71e990b3195ef03
Ronan Williams <ronan@ceterna.com>
2019-09-10 20:09:41 +0100

    10.09.19 2

--- src/aura/QuoteHolder/QuoteHolderController.js
+++ src/aura/QuoteHolder/QuoteHolderController.js
@@ -127,30 +127,67 @@
         var sourceData = $E.currentTarget.dataset;
 
 
-        console.log(sourceData.id);
+        console.log(Object.keys(sourceData));
+        console.log(Object.values(sourceData));
         console.log(sourceData.week);
 
 
+        var familyGroups = $C.get('v.familyGroups');
+
+        console.log('length si ' + familyGroups.length + ' and index is ' + sourceData.familyindex);
+
+
+        var newLineItem = {
+
+            Id: "00k0E000004wWvwQAE",
+            Name: "testSaasLicense1 Correspondent Banking AML",
+            PricebookEntryId: "01u0Y000004JA4AQAW",
+            Product2Id: "01t0Y000005LxpyQAC",
+            Product_Code__c: "IG",
+            Quantity: 1,
+            Term_Years__c: "3",
+            TotalPrice: 100,
+            UnitPrice: 100,
+            UsesAmount__c: false
+
+        };
+
+        //
+        familyGroups[sourceData.familyindex].UseCases[sourceData.usecaseindex].Products[sourceData.productindex].Entries.push(newLineItem);
+        //
+        //
+        $C.set('v.familyGroups',familyGroups);
+
         // console.log(source);
 
-        var insert = $C.get('c.insertLineItem');
-        insert.setParams({
-            recordId : $C.get('v.recordId'),
-            productId : sourceData.id,
-            pricebookEntryId : sourceData.pricebookEntryId,
-            year : sourceData.week
-        });
-        insert.setCallback(this,function (response) {
-
-
-            console.log(response.getReturnValue());
-
-
-
-        });
-
-        $A.enqueueAction(insert);
+        // var insert = $C.get('c.insertLineItem');
+        // insert.setParams({
+        //     recordId : $C.get('v.recordId'),
+        //     productId : sourceData.id,
+        //     pricebookEntryId : sourceData.pricebookEntryId,
+        //     year : sourceData.week
+        // });
+        // insert.setCallback(this,function (response) {
+        //
+        //
+        //     console.log(response.getReturnValue());
+        //
+        //
+        //
+        // });
+        //
+        // $A.enqueueAction(insert);
     },
+    setUsesAmount : function($C,$E,$H) {
+        var sourceData = $E.currentTarget.dataset;
+        var familyGroups = $C.get('v.familyGroups');
+
+
+        familyGroups[sourceData.familyindex].UseCases[sourceData.usecaseindex].Products[sourceData.productindex].Entries[sourceData.entryindex].UsesAmount__c =
+            !familyGroups[sourceData.familyindex].UseCases[sourceData.usecaseindex].Products[sourceData.productindex].Entries[sourceData.entryindex].UsesAmount__c;
+        $C.set('v.familyGroups',familyGroups);
+    },
+
     toggleVertical : function($C,$E,$H){
         $E.currentTarget.value = $C.get('v.vertical') === false ? 'on' : 'off';
         $C.set('v.vertical',!$C.get('v.vertical'));
